44f9edb3e0f404e10613c0b63b5e1c86728a02e5
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-15 23:03:51 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 import { Router } from './router.js'
 
+const menu = document.querySelectorAll('a')
+const pages = {
+  '/': 'home.html',
+  '/exploration': 'exploration.html',
+  '/universe': 'universe.html'
+}
+
 const router = new Router()
 
 router.add('/', '/pages/home.html')
@@ -10,3 +17,22 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+window.setBackgroundAndMenu = href => {
+  href = String(href)
+
+  menu.forEach(item => {
+    if (item.pathname === href) {
+      item.classList.add('active')
+    } else {
+      item.classList.remove('active')
+    }
+
+    if (href === '/') {
+      href = '/home'
+    }
+
+    document.querySelector(
+      '#app'
+    ).style.backgroundImage = `url('./assets${href}.png')`
+  })
+}
